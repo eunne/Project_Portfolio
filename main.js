18a9e034c9b5b970cd2146e07ab0121a5f8112d8
Eunhye, Son <85356011+eunne@users.noreply.github.com>
2022-03-18 06:06:39 +0900

    JS: fixed navbar and color change
    make navbar fixed and color chaged.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const toggleBtn = document.querySelector(".navbar__toggleBtn");
 const menu = document.querySelector(".navbar__menu");
 
@@ -5,3 +7,21 @@ toggleBtn.addEventListener("click", () => {
   toggleBtn.classList.toggle("clicked");
   menu.classList.toggle("active");
 })
+
+
+// 스크롤 시 navbar 색상 바꾸기
+const navbar = document.querySelector("#navbar"); //from HTML
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  //navbar의 높이가 현재까지 스크롤 된 Y값보다 작다면 navbar에 fixed라는 클래스를 넣어라.
+  navbarHeight < scrollY ? navbar.classList.add("fixed") : navbar.classList.remove("fixed");
+  
+  // if (scrollY > navbarHeight) {
+  //   navbar.classList.add("fixed");
+  // } else {
+  //   navbar.classList.remove("fixed");
+  // }
+});
