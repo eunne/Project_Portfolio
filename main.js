35b933a0c6dbe9f574a4965eda2d5cf13044b146
Eunhye, Son <85356011+eunne@users.noreply.github.com>
2022-03-19 09:21:16 +0900

    JS:scroll to selected pages

--- main.js
+++ main.js
@@ -25,3 +25,39 @@ document.addEventListener("scroll", () => {
   //   navbar.classList.remove("fixed");
   // }
 });
+
+
+//버튼을 누르면 해당 페이지로 이동
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const skillls = document.querySelector('#skills');
+const work = document.querySelector('#work');
+const tesimonials = document.querySelector('#testimonials');
+const contact = document.querySelector('#contact');
+
+home.addEventListener("click", () => {
+  const rect = home.getBoundingClientRect();
+  scrollTo(0,rect.y - navbarHeight);
+});
+
+about.addEventListener("click", () => {
+  const rect = about.getBoundingClientRect();
+  console.log(scrollTo(0,rect.y - navbarHeight));
+});
+
+//
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+
+  const target = event.target;
+  const link = target.dataset.link;
+
+  console.log(event.target.dataset.link);
+  
+  if (link == null) {
+    return;
+  } else {
+    const scroll = document.querySelector(link);
+    scroll.scrollIntoView({behavior:"smooth"});
+  }
+});
