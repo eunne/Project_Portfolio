4f721bb471dd4bd92f96451a0a1d5a2c4d513564
Eunhye, Son <85356011+eunne@users.noreply.github.com>
2022-03-24 05:18:51 +0900

    JS: navbarmenu closed UI

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 'use strict';
 
-const toggleBtn = document.querySelector(".navbar__toggleBtn");
-const menu = document.querySelector(".navbar__menu");
-
-toggleBtn.addEventListener("click", () => {
-  toggleBtn.classList.toggle("clicked");
-  menu.classList.toggle("active");
-})
 
 
 // 스크롤 시 navbar 색상 바꾸기
@@ -15,30 +8,37 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
   const scrollY = window.scrollY;
-
+  
   //navbar의 높이가 현재까지 스크롤 된 Y값보다 작다면 navbar에 fixed라는 클래스를 넣어라.
   navbarHeight < scrollY ? navbar.classList.add("fixed") : navbar.classList.remove("fixed");
   
   // if (scrollY > navbarHeight) {
-  //   navbar.classList.add("fixed");
-  // } else {
-  //   navbar.classList.remove("fixed");
-  // }
-});
-
-//
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-  
-  const target = event.target;
-  const link = target.dataset.link;
-  
-  if (link == null) {
-    return;
-  } else {
-    scrollIntoView(link);
-  }
-});
+    //   navbar.classList.add("fixed");
+    // } else {
+      //   navbar.classList.remove("fixed");
+      // }
+    });
+    
+    //
+    const navbarMenu = document.querySelector('.navbar__menu');
+    navbarMenu.addEventListener('click', (event) => {
+      
+      const target = event.target;
+      const link = target.dataset.link;
+      
+      if (link == null) {
+        return;
+      } else {
+        scrollIntoView(link);
+        navbarMenu.classList.remove('active');
+      }
+    });
+    
+    const toggleBtn = document.querySelector(".navbar__toggleBtn");
+    
+    toggleBtn.addEventListener("click", () => {
+      navbarMenu.classList.toggle("active");
+    });
 
 
 // handle click on 'contact me' btn on home 
@@ -126,7 +126,6 @@ workCategories.addEventListener('click', (event) => {
   }, 300);
 
 });
-
 
 
 // scrollintoview 함수만들기
